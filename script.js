3ca88c68c36d34fb0f033ce8d139b71427e8da71
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-17 09:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let ele = null
+
+document.querySelectorAll(".image").forEach(image => {
+	image.addEventListener("dragstart" , (e) =>{
+		ele = e.target;
+	});
+
+	image.addEventListener("dragover" , (e) =>{
+		e.preventDefault();
+	})
+
+	image.addEventListener("drop", (e)={
+		e.preventDefault();
+		if(ele != e.target){
+			let temp = ele.style.backgroundImage;
+			ele.style.backgroundImage = e.target.style.backgroundImage;
+			e.target.style.backgroundImage = temp;
+		}
+	})
+})
